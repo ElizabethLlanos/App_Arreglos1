e5182236c0c3c205ea323a70afa9bda47f5a2162
ElizabethLlanos <85859478+ElizabethLlanos@users.noreply.github.com>
2021-06-22 14:50:32 -0500

    App1_Arreglos
    En esta primera aplicación se reciben 10 números enteros, se almacenan en un arreglo y con ellos se calcula promedio, sumatoria, número mayor y número menor.

---
+++ Aplicacion1Arreglos/Numeros.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion1Arreglos
+{
+    class Numeros
+    {
+        //Atributos
+        private int suma;
+        //Método para sumar los números, se recibe el array por parámetro
+        public int Suma(int[] Num)
+        {
+            //Se crea un array con 10 espacios
+            int[] numero = new int[10];
+            //se rellena el array con los datos recibidos por parámetro
+            numero = Num;
+            suma = 0;
+            //Se utiliza un for para recorrer y sumar los datos del array
+            for(int i = 0; i < 10; i++)
+            {
+                suma += numero[i];
+            }
+            return suma;
+        }
+        //Método para calcular promedio
+        public int Promedio(int[] Num)
+        {
+            int[] numero = new int[10];
+            numero = Num;
+            int promedio = 0;
+            //Se calcula el promedio
+            promedio = suma / 10;
+
+            return promedio;
+        }
+        //Método para encontrar el número mayor
+        public int Num_mayor(int[] Num)
+        {
+            int[] numero = new int[10];
+            numero = Num;
+            int num_mayor = 0;
+            //Se ordenan los datos del array de menor a mayor
+            Array.Sort(numero);
+            //El mayor será el de la última posición
+            num_mayor = numero[9];
+
+            return num_mayor;
+        }
+        //Método para encontrar el número menor
+        public int Num_menor(int[] Num)
+        {
+            int[] numero = new int[10];
+            numero = Num;
+            int num_menor = 0;
+            //Se ordenan los numeros de menor a mayor
+            Array.Sort(numero);
+            //El menor será el de la primera posición
+            num_menor = numero[0];
+
+            return num_menor;
+        }
+
+
+    }
+}
---
+++ Aplicacion1Arreglos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Aplicacion1Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancia
+            Numeros objNumero = new Numeros();
+            //Array para guardar los datos otorgados por el usuario
+            int[] Num = new int[10];
+            Console.WriteLine("♠♠♠♠♠ NÚMEROS ENTEROS♠♠♠♠♠");
+            Console.WriteLine(">Introduce 10 números:");
+            //Con el ciclo for se recibirán los números y se guardarán en el array
+            for(int j = 0; j < 10; j++)
+            {
+                Console.Write("► ");
+                Num[j] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+            //Llamada al método suma
+            int suma = objNumero.Suma(Num) ;
+            Console.WriteLine($"►La suma de los numeros anteriores es: {suma}");
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+            //Llamada al método promedio
+            int promedio = objNumero.Promedio(Num);
+            Console.WriteLine($"►El promedio es: {promedio}");
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+            //Llamada al método Num_mayor
+            int num_mayor = objNumero.Num_mayor(Num);
+            Console.WriteLine($"►El numero mayor es: {num_mayor}");
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+            //Llamada al método Num_menor
+            int num_menor = objNumero.Num_menor(Num);
+            Console.WriteLine($"►El numero menor es: {num_menor}");
+            Console.WriteLine("♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦♦");
+
+            Console.ReadKey();
+
+        }
+    }
+}
